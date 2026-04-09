2f6727784871e0d4da49b63c045664d4320c4fa7
Kunihisa Matsuda <love_coke@mac.com>
2026-04-09 20:40:41 +0900

    magazine dir

---
+++ boards/magazine/magazine.scm
@@ -0,0 +1,2 @@
+(("ナンプレ雑誌 Q001" "./boards/magazine/Q001.scm"))
+
--- config.scm
+++ config.scm
@@ -1,1 +1,2 @@
-((daiso . ((note . "DAISO ナンプレ") (path . "./boards/daiso/daiso.scm"))))
+((daiso . ((note . "DAISO ナンプレ") (path . "./boards/daiso/daiso.scm")))
+ (magazine . ((note . "ナンプレ雑誌") (path . "./boards/magazine/magazine.scm"))))
